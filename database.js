34b731d55ed079d8520461fe283704ed044033b5
Kurax <gmmgmm@gmail.com>
2015-12-01 17:19:51 +0800

    增加数据库访问组件 严正抗议阿里云ACE的NODE环境不支持ES6

---
+++ database.js
@@ -0,0 +1,50 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(process.env.FLEET_CONN_STR, {
+    define: {
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        underscored: false,
+        freezeTableName: true,
+        timestamps: true,
+        paranoid: true
+    }
+});
+
+const db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+};
+
+var model;
+// FleetUser
+model = sequelize.define('FleetUser', {
+    id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4
+    },
+    duoshuoId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    defaultScope: {
+        where: {
+            deletedAt: null
+        }
+    }
+});
+db[model.getTableName()] = model;
+
+sequelize.sync().then(function() {
+    console.log('Database initialized.');
+});
+
+module.exports = function(app) {
+    app.db = db;
+};
--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-const server = app.listen(0, function() {
-    console.log('Express server started on port %s', server.address().port);
-});
+const database = require('./database');
+database(app);
+
+var server;
+if(app.get('env') === 'production') {
+    server = app.listen(0, function() {
+        console.log('Server started on port ' + server.address().port);
+    });
+} else {
+    server = app.listen(8080, 'localhost', function() {
+        const address = 'http://' + server.address().address + ':' + server.address().port;
+        console.log('Server started on %s', address);
+        require('child_process').execSync('start ' + address);
+    });
+}
